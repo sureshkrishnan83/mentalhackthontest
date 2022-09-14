82f88d0c80a0ec2b0cb901182224763dcefd5c40
sureshkrishnan83 <sureshkrishnan83@outlook.com>
2022-09-14 12:40:38 +0800

    Testing new reactive app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Instructions from './Instructions.js';
+import './App.css';
+const displayEmojiName = event => alert(event.target.id)
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  }
+];
+
+function App() {
+  const greetings = "greetings"
+  const displaypara = false
+   return(
+    <div className="container">
+    <h1 id= {greetings}>Hello, World</h1>
+    {displaypara && <p>I am writing JSX</p>}
+    <Instructions />
+    <ul>
+       {
+        emojis.map(emoji => (
+          <li key={emoji.name}>
+            <button
+              onClick = {displayEmojiName}>
+              <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+            </button>
+
+          </li>
+        ))
+       } 
+      </ul>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Instructions.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class Instructions extends Component {
+
+  render() {
+    return(
+      <>
+        <img alt="laughing crying emoji" src='./emoji.svg' />
+        <p>Click on an emoji to view the emoji short name.</p>
+      </>
+    )
+  }
+}
